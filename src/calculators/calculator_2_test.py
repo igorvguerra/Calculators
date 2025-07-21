e00b734e9d9f92df197dc9a47f35130099255c75
Igor <igor.vguerra@hotmail.com>
2025-07-21 14:29:58 +0100

    implementing the 2nd calculator

---
+++ src/calculators/calculator_2_test.py
@@ -0,0 +1,17 @@
+from .calculator_2 import Calculator2
+from typing import Dict
+
+class MockRequest:
+    def __init__(self, body: Dict):
+        self.json = body
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+
+    calculator_2 = Calculator2()
+    formated_response =  calculator_2.calculate(mock_request)
+    print()
+    print(formated_response)
+
+    assert isinstance(formated_response, dict)
+    assert formated_response == {'data': {'Calculator': 2, 'Result': 0.08}}
