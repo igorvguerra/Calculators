4a51dc8d00e55bcaa276f7302d3d4f35d20a7725
Igor <igor.vguerra@hotmail.com>
2025-07-22 13:29:34 +0100

    creating an interface for the drivers

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,5 +1,6 @@
 from .calculator_2 import Calculator2
 from typing import Dict
+from src.drivers.numpy_handler import NumpyHandler
 
 class MockRequest:
     def __init__(self, body: Dict):
@@ -8,7 +9,8 @@ class MockRequest:
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
 
-    calculator_2 = Calculator2()
+    driver = NumpyHandler()
+    calculator_2 = Calculator2(driver)
     formated_response =  calculator_2.calculate(mock_request)
     print()
     print(formated_response)
